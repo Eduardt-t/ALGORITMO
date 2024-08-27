38363021a3f3a7cabc2808430cb5b0fdabb3548e
Eduardt-t <etoledo22@alumnos.utalca.cl>
2024-08-27 10:11:05 -0400

    hola

---
+++ paciente_arreglo.cpp
@@ -0,0 +1,5 @@
+Paciente pacientes[3] = {paciente1, paciente2, paciente3};
+for(int i = 0; i < 3; i++) {
+    std::cout << "Paciente " << i+1 << ": " << pacientes[i].nombre << ", " << pacientes[i].edad << " años, " << pacientes[i].peso << " kg, " << pacientes[i].altura << " m\n";
+}
+ 
