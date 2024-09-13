75d02ae8499fcddca8df1140c34354745279f7c0
Eduardt-t <etoledo22@alumnos.utalca.cl>
2024-09-12 23:15:32 -0300

    Update actividad2.cpp
    actividad 2 del laboratorio 3

--- actividad2.cpp
+++ actividad2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <stack>
 #include <vector>
+#include <stack>
 #include <string>
 
 using namespace std;
@@ -10,93 +10,212 @@ struct Contenedor {
     string empresa;
 };
 
-int EncontrarPilaConContenedor(vector<stack<Contenedor>>& pilas, int numero) {
-    for (size_t i = 0; i < pilas.size(); ++i) {
-        stack<Contenedor> tempStack;
-        while (!pilas[i].empty()) {
-            if (pilas[i].top().numero == numero) {
-                // Devolver los contenedores a la pila original
-                while (!tempStack.empty()) {
-                    pilas[i].push(tempStack.top());
-                    tempStack.pop();
-                }
-                return i;
+class PilaContenedores {
+    stack<Contenedor> pila;
+    int capacidad;
+
+public:
+    PilaContenedores(int cap) : capacidad(cap) {}
+
+    bool estaLlena() const {
+        return pila.size() >= capacidad;
+    }
+
+    bool estaVacia() const {
+        return pila.empty();
+    }
+
+    bool agregarContenedor(const Contenedor& cont) {
+        if (estaLlena()) {
+            return false;
+        }
+        pila.push(cont);
+        return true;
+    }
+
+    bool retirarContenedor(int numero) {
+        stack<Contenedor> aux;
+        bool encontrado = false;
+
+        while (!pila.empty()) {
+            if (pila.top().numero == numero) {
+                encontrado = true;
+                break;
             }
-            tempStack.push(pilas[i].top());
-            pilas[i].pop();
+            aux.push(pila.top());
+            pila.pop();
         }
-        // Devolver los contenedores a la pila original
-        while (!tempStack.empty()) {
-            pilas[i].push(tempStack.top());
-            tempStack.pop();
+
+        while (!aux.empty()) {
+            pila.push(aux.top());
+            aux.pop();
         }
-    }
-    return -1;
-}
 
-void MostrarPila(stack<Contenedor> pila) {
-    if (pila.empty()) {
-        cout << "La pila está vacía" << endl;
-        return;
-    }
-    while (!pila.empty()) {
-        Contenedor c = pila.top();
-        cout << "Contenedor " << c.numero << "/" << c.empresa << endl;
-        pila.pop();
-    }
-}
+        if (encontrado) {
+            pila.pop();
+        }
 
-void RetirarContenedor(vector<stack<Contenedor>>& pilas, int numero, size_t n) {
-    int pilaIndex = EncontrarPilaConContenedor(pilas, numero);
-    if (pilaIndex == -1) {
-        cout << "El contenedor no se encontró en ninguna pila." << endl;
-        return;
+        return encontrado;
     }
 
-    stack<Contenedor> pilaAux;
+    Contenedor obtenerContenedor() const {
+        if (!pila.empty()) {
+            return pila.top();
+        }
+        return Contenedor{-1, ""};  // Retorna un contenedor vacío en caso de pila vacía
+    }
 
-    // Remover contenedores encima del contenedor retirado
-    while (!pilas[pilaIndex].empty()) {
-        if (pilas[pilaIndex].top().numero == numero) {
-            pilas[pilaIndex].pop();
-            cout << "Contenedor " << numero << " retirado" << endl;
-            break;
-        } else {
-            pilaAux.push(pilas[pilaIndex].top());
-            pilas[pilaIndex].pop();
+    void mostrarPila() const {
+        stack<Contenedor> aux = pila;
+        while (!aux.empty()) {
+            Contenedor cont = aux.top();
+            cout << "Contenedor " << cont.numero << "/" << cont.empresa << endl;
+            aux.pop();
+        }
+        if (pila.empty()) {
+            cout << "La pila está vacía" << endl;
         }
     }
+};
 
-    // Mover los contenedores retirados a otras pilas con espacio
-    while (!pilaAux.empty()) {
-        bool movido = false;
-        for (size_t j = 0; j < pilas.size(); ++j) {
-            if (j != pilaIndex && pilas[j].size() < n) {
-                pilas[j].push(pilaAux.top());
-                pilaAux.pop();
-                movido = true;
+class GestorPilas {
+    vector<PilaContenedores> pilas;
+    int capacidad;
+
+public:
+    GestorPilas(int numPilas, int cap) : capacidad(cap) {
+        for (int i = 0; i < numPilas; ++i) {
+            pilas.emplace_back(cap);
+        }
+    }
+
+    void agregarContenedores() {
+        int numContenedores;
+        cout << "Ingrese el número de contenedores a agregar: ";
+        cin >> numContenedores;
+
+        if (numContenedores <= 0) {
+            cout << "Número de contenedores no válido." << endl;
+            return;
+        }
+
+        for (int i = 0; i < numContenedores; ++i) {
+            if (i >= 1 && todasPilasLlenas()) {
+                cout << "No hay espacio en ninguna pila para agregar nuevos contenedores." << endl;
+                return;
+            }
+
+            Contenedor cont;
+            cout << "Ingrese el número del contenedor: ";
+            cin >> cont.numero;
+            cout << "Ingrese el nombre de la empresa: ";
+            cin >> cont.empresa;
+
+            bool agregado = false;
+            for (auto& pila : pilas) {
+                if (pila.agregarContenedor(cont)) {
+                    agregado = true;
+                    break;
+                }
+            }
+
+            if (!agregado) {
+                cout << "No se pudo agregar el contenedor " << cont.numero << " en ninguna pila." << endl;
+            }
+        }
+    }
+
+    void retirarContenedor() {
+        int numero;
+        cout << "Ingrese el número del contenedor a retirar: ";
+        cin >> numero;
+
+        vector<Contenedor> moverContenedores;
+        bool encontrado = false;
+
+        // Buscar el contenedor y almacenar los que están encima
+        for (auto& pila : pilas) {
+            while (!pila.estaVacia() && pila.obtenerContenedor().numero != numero) {
+                moverContenedores.push_back(pila.obtenerContenedor());
+                pila.retirarContenedor(pila.obtenerContenedor().numero);
+            }
+            if (!pila.estaVacia() && pila.obtenerContenedor().numero == numero) {
+                encontrado = true;
+                pila.retirarContenedor(numero);
                 break;
             }
         }
-        if (!movido) {
-            cout << "No hay espacio disponible para los contenedores." << endl;
-            break;
+
+        if (!encontrado) {
+            cout << "El contenedor " << numero << " no se encontró." << endl;
+            return;
+        }
+
+        // Intentar reubicar los contenedores
+        for (auto& cont : moverContenedores) {
+            bool reubicado = false;
+            for (auto& pila : pilas) {
+                if (!pila.estaLlena()) {
+                    pila.agregarContenedor(cont);
+                    reubicado = true;
+                    break;
+                }
+            }
+            if (!reubicado) {
+                cout << "No se pudo mover algunos contenedores a otras pilas. Contenedores que no se movieron:" << endl;
+                for (const auto& c : moverContenedores) {
+                    cout << "Contenedor " << c.numero << "/" << c.empresa << endl;
+                }
+                mostrarPrimerosContenedores();
+                cout << "Por favor, asegúrese de tener espacio en las pilas antes de intentar retirar más contenedores." << endl;
+                return;
+            }
+        }
+
+        cout << "Contenedor " << numero << " retirado." << endl;
+    }
+
+    void mostrarPilas() const {
+        for (size_t i = 0; i < pilas.size(); ++i) {
+            cout << "Pila " << (i + 1) << ":" << endl;
+            pilas[i].mostrarPila();
         }
     }
-}
+
+    void mostrarPrimerosContenedores() const {
+        cout << "Primeros contenedores en cada pila:" << endl;
+        for (size_t i = 0; i < pilas.size(); ++i) {
+            if (!pilas[i].estaVacia()) {
+                Contenedor cont = pilas[i].obtenerContenedor();
+                cout << "Pila " << (i + 1) << ": Contenedor " << cont.numero << "/" << cont.empresa << endl;
+            } else {
+                cout << "Pila " << (i + 1) << ": La pila está vacía" << endl;
+            }
+        }
+    }
+
+private:
+    bool todasPilasLlenas() const {
+        for (const auto& pila : pilas) {
+            if (!pila.estaLlena()) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
 
 int main() {
-    size_t n, m;
-
+    int n, m;
     cout << "Ingrese el número máximo de contenedores por pila (n): ";
     cin >> n;
     cout << "Ingrese el número máximo de pilas (m): ";
     cin >> m;
 
-    vector<stack<Contenedor>> pilas(m);
+    GestorPilas gestor(m, n);
 
-    int opcion;
-    do {
+    while (true) {
+        int opcion;
         cout << "Ingrese operación:" << endl;
         cout << "Agregar contenedor [1]" << endl;
         cout << "Retirar contenedor [2]" << endl;
@@ -106,51 +225,20 @@ int main() {
         cout << "Opción: ";
         cin >> opcion;
 
-        if (opcion == 1) {
-            int numContenedores;
-            cout << "Ingrese el número de contenedores a agregar: ";
-            cin >> numContenedores;
-
-            for (int i = 0; i < numContenedores; ++i) {
-                Contenedor c;
-                cout << "Ingrese el número del contenedor: ";
-                cin >> c.numero;
-                cout << "Ingrese el nombre de la empresa: ";
-                cin >> c.empresa;
-
-                bool agregado = false;
-                for (size_t j = 0; j < pilas.size(); ++j) {
-                    if (pilas[j].size() < n) {
-                        pilas[j].push(c);
-                        agregado = true;
-                        break;
-                    }
-                }
-
-                if (!agregado) {
-                    cout << "No hay espacio en ninguna pila para el contenedor " << c.numero << endl;
-                }
-            }
-
-        } else if (opcion == 2) {
-            int numero;
-            cout << "Ingrese el número del contenedor a retirar: ";
-            cin >> numero;
-            RetirarContenedor(pilas, numero, n);
-        } else if (opcion == 3) {
-            for (size_t i = 0; i < pilas.size(); ++i) {
-                cout << "Pila " << i + 1 << ":" << endl;
-                MostrarPila(pilas[i]);
-            }
+        switch (opcion) {
+            case 1:
+                gestor.agregarContenedores();
+                break;
+            case 2:
+                gestor.retirarContenedor();
+                break;
+            case 3:
+                gestor.mostrarPilas();
+                break;
+            case 0:
+                return 0;
+            default:
+                cout << "Opción no válida." << endl;
         }
-
-        if (opcion != 0) {
-            cout << "Presione cualquier tecla para continuar ..." << endl;
-            cin.ignore();
-            cin.get();
-        }
-
-    } while (opcion != 0);
-
-    return 0;
+    }
 }
